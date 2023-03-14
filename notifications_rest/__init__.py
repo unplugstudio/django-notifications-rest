2a49d531bb769aafaec2db2cdcb16858f1119cd6
androso <androso421@gmail.com>
2023-03-14 09:24:40 -0600

    Updating package version

--- notifications_rest/__init__.py
+++ notifications_rest/__init__.py
@@ -6,6 +6,6 @@
 """
 
 # PEP 386-compliant version number: N.N[.N]+[{a|b|c|rc}N[.N]+][.postN][.devN]
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 default_app_config = 'notifications_rest.apps.NotificationsRestConfig'
